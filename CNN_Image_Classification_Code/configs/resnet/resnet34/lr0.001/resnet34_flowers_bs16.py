3ce6fef01b58e5d5d56c7dc45f2e57683e8192e5
timmychan823 <chanshunhei09@gmail.com>
2024-02-24 14:14:01 +0800

    added resnet34

---
+++ CNN_Image_Classification_Code/configs/resnet/resnet34/lr0.001/resnet34_flowers_bs16.py
@@ -0,0 +1,4 @@
+_base_ = [
+        '../../../_base_/models/resnet34_flowers.py', '../../../_base_/datasets/flowers_bs16.py',
+    '../../../_base_/schedules/resnet34/lr0.001/flowers_bs32.py', '../../../_base_/default_runtime.py'
+]
