c0be5d9bca7370c125be8431f4162932cbac897b
ambr-0-se <100066314+ambr-0-se@users.noreply.github.com>
2024-02-26 13:14:12 +0000

    Change the model config

--- CNN_Image_Classification_Code/configs/_base_/models/vgg19_flowers.py
+++ CNN_Image_Classification_Code/configs/_base_/models/vgg19_flowers.py
@@ -2,10 +2,9 @@
 model = dict(
     type='ImageClassifier',
     backbone=dict(
-        type='VGG', depth=19, norm_cfg=dict(type='BN'), num_classes=17),
+        type='VGG', depth=19, norm_cfg=dict(type='BN',requires_grad=True), num_classes=17),
     neck=None,
     head=dict(
         type='ClsHead',
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        topk=(1, 5),
     ))
