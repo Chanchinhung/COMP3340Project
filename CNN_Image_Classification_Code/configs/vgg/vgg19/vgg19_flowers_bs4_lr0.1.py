ad2c68b7189cc95d24263a66bdd1e8caaa510ba0
ambr-0-se <100066314+ambr-0-se@users.noreply.github.com>
2024-02-26 14:37:45 +0000

    Add "pillow" to resize the image

--- CNN_Image_Classification_Code/configs/vgg/vgg19/vgg19_flowers_bs4_lr0.1.py
+++ CNN_Image_Classification_Code/configs/vgg/vgg19/vgg19_flowers_bs4_lr0.1.py
@@ -1,5 +1,5 @@
 _base_ = [
         '../../_base_/models/vgg19_flowers.py', 
-        '../../_base_/datasets/flowers_bs4.py',
+        '../../_base_/datasets/flowers_bs4_vgg.py',
         '../../_base_/schedules/vgg19/lr0.1/flowers_bs32.py', 
         '../../_base_/default_runtime.py' ]
