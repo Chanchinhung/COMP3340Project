6e7e3370d43782c77ea11d3f6bd7c72e6e60330e
ambr-0-se <100066314+ambr-0-se@users.noreply.github.com>
2024-02-26 10:06:45 +0000

    Modify the path in the file in config files for vgg19

--- CNN_Image_Classification_Code/configs/vgg/vgg19/vgg19_flowers_bs4_lr0.01.py
+++ CNN_Image_Classification_Code/configs/vgg/vgg19/vgg19_flowers_bs4_lr0.01.py
@@ -1,5 +1,5 @@
 _base_ = [
         'CNN_Image_Classification_Code/configs/_base_/models/vgg19_flowers.py', 
-        '../_base_/datasets/flowers_bs4.py',
+        'CNN_Image_Classification_Code/configs/_base_/datasets/flowers_bs4.py',
         'CNN_Image_Classification_Code/configs/_base_/schedules/vgg19/lr0.01/flowers_bs32.py', 
-        '../_base_/default_runtime.py' ]
+        'CNN_Image_Classification_Code/configs/_base_/default_runtime.py' ]
