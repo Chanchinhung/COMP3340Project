a6781ef615ee5761cdc4ee50e0242e885b84eb5f
Zay Chan <zaychan80@gmail.com>
2024-03-27 21:50:12 +0800

    random rotate imported with torchvision

--- CNN_Image_Classification_Code/configs/_base_/datasets/dataaugment/flowers_bs16_randrotate.py
+++ CNN_Image_Classification_Code/configs/_base_/datasets/dataaugment/flowers_bs16_randrotate.py
@@ -5,7 +5,12 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RandomRotation', degrees=15),
+    dict(
+        type='RandomApply',
+        transforms=[
+            dict(type='Rotate', angle_range=(-15, 15)),
+        ],
+        p=0.5),
     dict(type='RandomResizedCrop', size=224),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='Normalize', **img_norm_cfg),
