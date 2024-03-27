50d9c611f9a68ebf78c2cce9be7fafc7ae4e089b
Chan Chin Hung <zaychan@gpu2-comp-109.gpufarm2.cs.hku.hk>
2024-03-28 00:53:20 +0800

    trained and tested data augmentation rotate resnet50 bs16

--- CNN_Image_Classification_Code/output/dataaugment/resnet50_flowers_bs16_randrotate/resnet50_flowers_bs16_randrotate.py
+++ CNN_Image_Classification_Code/output/dataaugment/resnet50_flowers_bs16_randrotate/resnet50_flowers_bs16_randrotate.py
@@ -17,7 +17,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='RandomRotate', rotate_prob=0.5, max_angle=15),
+    dict(type='Rotate', angle=15.0),
     dict(type='RandomResizedCrop', size=224),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(
@@ -50,7 +50,7 @@ data = dict(
         ann_file='data/flowers/meta/train.txt',
         pipeline=[
             dict(type='LoadImageFromFile'),
-            dict(type='RandomRotate', rotate_prob=0.5, max_angle=15),
+            dict(type='Rotate', angle=15.0),
             dict(type='RandomResizedCrop', size=224),
             dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
             dict(
