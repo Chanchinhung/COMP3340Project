c41fec157b51688bdc25807efd88f10a7e02ae26
Zay Chan <zaychan80@gmail.com>
2024-03-27 23:42:20 +0800

    debugging dataaugmentation of random rescale

--- CNN_Image_Classification_Code/configs/_base_/datasets/dataaugment/flowers_bs16_randrotate.py
+++ CNN_Image_Classification_Code/configs/_base_/datasets/dataaugment/flowers_bs16_randrotate.py
@@ -5,7 +5,7 @@ img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
-    dict(type='Rotate', angle=random()*180),
+    dict(type='Rotate', angle=random()*180), #rotation of image
     dict(type='RandomResizedCrop', size=224),
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='Normalize', **img_norm_cfg),
