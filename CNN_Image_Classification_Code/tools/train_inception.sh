7cec9aa9269138bd32b906947952ed95698e1c30
ambr-0-se <100066314+ambr-0-se@users.noreply.github.com>
2024-04-03 18:57:25 +0000

    Add script to train inception model

---
+++ CNN_Image_Classification_Code/tools/train_inception.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Command 1
+#echo "Training inception_v3_flowers_bs4_lr0.1"
+#python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs4_lr0.1.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs4_lr0.1'
+
+echo "Training inception_v3_flowers_bs4_lr0.01"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs4_lr0.01.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs4_lr0.01'
+
+echo "Training inception_v3_flowers_bs4_lr0.001"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs4_lr0.001.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs4_lr0.001'
+
+echo "Training inception_v3_flowers_bs4_lr0.0001"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs4_lr0.0001.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs4_lr0.0001'
+
+echo "Training inception_v3_flowers_bs16_lr0.1"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs16_lr0.1.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs16_lr0.1'
+
+echo "Training inception_v3_flowers_bs16_lr0.01"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs16_lr0.01.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs16_lr0.01'
+
+echo "Training inception_v3_flowers_bs16_lr0.001"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs16_lr0.001.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs16_lr0.001'
+
+echo "Training inception_v3_flowers_bs16_lr0.0001"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs16_lr0.0001.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs16_lr0.0001'
+
+echo "Training inception_v3_flowers_bs64_lr0.1"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs64_lr0.1.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs64_lr0.1'
+
+echo "Training inception_v3_flowers_bs64_lr0.01"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs64_lr0.01.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs64_lr0.01'
+
+echo "Training inception_v3_flowers_bs64_lr0.001"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs64_lr0.001.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs64_lr0.001'
+
+echo "Training inception_v3_flowers_bs64_lr0.0001"
+python tools/train.py \
+	--config 'configs/inception/inception_v3/inceptionv3_flowers_bs64_lr0.0001.py' \
+	--work-dir 'output/inception_v3/inception_v3_flowers_bs64_lr0.0001'
