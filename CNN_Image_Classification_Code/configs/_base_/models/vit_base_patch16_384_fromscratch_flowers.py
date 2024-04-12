927a1ce933aa652e796de91ed120cf472598cc5c
Angus <114798767+angusf777@users.noreply.github.com>
2024-04-12 11:47:29 +0800

    correct syntax error

--- CNN_Image_Classification_Code/configs/_base_/models/vit_base_patch16_384_fromscratch_flowers.py
+++ CNN_Image_Classification_Code/configs/_base_/models/vit_base_patch16_384_fromscratch_flowers.py
@@ -12,7 +12,7 @@ model = dict(
         feedforward_channels=3072,
         drop_rate=0.1,
         attn_drop_rate=0.,
-        arch ='base'
+        arch ='base',
         drop_path_rate=0.3
         ),
     neck=None,
