fd5d450d9e6d38db3a6812da47ff226b7f5697c8
timmychan823 <chanshunhei09@gmail.com>
2024-04-14 22:26:37 +0800

    updated shell script foro resnet34

--- CNN_Image_Classification_Code/tools/test_resnet34.sh
+++ CNN_Image_Classification_Code/tools/test_resnet34.sh
@@ -1,63 +1,63 @@
 #!/bin/bash
 echo "Testing resnet34_bs4_lr0.1"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs4.py' \
-  --checkpoint 'output/resnet34/bs4/lr0.1/latest.pth' \
-  --out 'output/resnet34/bs4/lr0.1/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs4.py' \
+    --checkpoint 'output/resnet34/bs4/lr0.1/latest.pth' \
+    --out 'output/resnet34/bs4/lr0.1/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs4_lr0.01"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs4.py' \
-  --checkpoint 'output/resnet34/bs4/lr0.01/latest.pth' \
-  --out 'output/resnet34/bs4/lr0.01/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs4.py' \
+    --checkpoint 'output/resnet34/bs4/lr0.01/latest.pth' \
+    --out 'output/resnet34/bs4/lr0.01/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs4_lr0.001"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs4.py' \
-  --checkpoint 'output/resnet34/bs4/lr0.001/latest.pth' \
-  --out 'output/resnet34/bs4/lr0.001/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs4.py' \
+    --checkpoint 'output/resnet34/bs4/lr0.001/latest.pth' \
+    --out 'output/resnet34/bs4/lr0.001/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs16_lr0.1"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs16.py' \
-  --checkpoint 'output/resnet34/bs16/lr0.1/latest.pth' \
-  --out 'output/resnet34/bs16/lr0.1/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs16.py' \
+    --checkpoint 'output/resnet34/bs16/lr0.1/latest.pth' \
+    --out 'output/resnet34/bs16/lr0.1/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs16_lr0.01"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs16.py' \
-  --checkpoint 'output/resnet34/bs16/lr0.01/latest.pth' \
-  --out 'output/resnet34/bs16/lr0.01/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs16.py' \
+    --checkpoint 'output/resnet34/bs16/lr0.01/latest.pth' \
+    --out 'output/resnet34/bs16/lr0.01/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs16_lr0.001"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs16.py' \
-  --checkpoint 'output/resnet34/bs16/lr0.001/latest.pth' \
-  --out 'output/resnet34/bs16/lr0.001/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs16.py' \
+    --checkpoint 'output/resnet34/bs16/lr0.001/latest.pth' \
+    --out 'output/resnet34/bs16/lr0.001/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs64_lr0.1"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs64.py' \
-  --checkpoint 'output/resnet34/bs64/lr0.1/latest.pth' \
-  --out 'output/resnet34/bs64/lr0.1/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs64.py' \
+    --checkpoint 'output/resnet34/bs64/lr0.1/latest.pth' \
+    --out 'output/resnet34/bs64/lr0.1/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs64_lr0.01"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs64.py' \
-  --checkpoint 'output/resnet34/bs64/lr0.01/latest.pth' \
-  --out 'output/resnet34/bs64/lr0.01/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs64.py' \
+    --checkpoint 'output/resnet34/bs64/lr0.01/latest.pth' \
+    --out 'output/resnet34/bs64/lr0.01/test.json' \
+    --metrics 'accuracy'
 
 echo "Testing resnet34_bs64_lr0.001"
 python tools/test.py \
-  --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs64.py' \
-  --checkpoint 'output/resnet34/bs64/lr0.001/latest.pth' \
-  --out 'output/resnet34/bs64/lr0.001/test.json' \
-  --metrics 'accuracy'
+    --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs64.py' \
+    --checkpoint 'output/resnet34/bs64/lr0.001/latest.pth' \
+    --out 'output/resnet34/bs64/lr0.001/test.json' \
+    --metrics 'accuracy'
--- CNN_Image_Classification_Code/tools/train_resnet34.sh
+++ CNN_Image_Classification_Code/tools/train_resnet34.sh
@@ -3,48 +3,48 @@
 
 echo "Training resnet34_bs4_lr0.1"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs4.py' \
-  --work-dir 'output/resnet34/bs4/lr0.1'\
+	--config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs4.py' \
+	--work-dir 'output/resnet34/bs4/lr0.1'
 
 echo "Training resnet34_bs4_lr0.01"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs4.py' \
-  --work-dir 'output/resnet34/bs4/lr0.01'\
+	--config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs4.py' \
+	--work-dir 'output/resnet34/bs4/lr0.01'
 
 echo "Training resnet34_bs4_lr0.001"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs4.py' \
-  --work-dir 'output/resnet34/bs4/lr0.001'\
+	--config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs4.py' \
+	--work-dir 'output/resnet34/bs4/lr0.001'
 
 echo "Training resnet34_bs16_lr0.1"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs16.py' \
-  --work-dir 'output/resnet34/bs16/lr0.1'\
+	--config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs16.py' \
+	--work-dir 'output/resnet34/bs16/lr0.1'
 
 echo "Training resnet34_bs16_lr0.01"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs16.py' \
-  --work-dir 'output/resnet34/bs16/lr0.01'\
+	--config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs16.py' \
+	--work-dir 'output/resnet34/bs16/lr0.01'
 
 echo "Training resnet34_bs16_lr0.001"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs16.py' \
-  --work-dir 'output/resnet34/bs16/lr0.001'
+	--config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs16.py' \
+	--work-dir 'output/resnet34/bs16/lr0.001'
 
 echo "Training resnet34_bs64_lr0.1"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs64.py' \
-  --work-dir 'output/resnet34/bs64/lr0.1'\
+	--config 'configs/resnet/resnet34/lr0.1/resnet34_flowers_bs64.py' \
+	--work-dir 'output/resnet34/bs64/lr0.1'
 
 echo "Training resnet34_bs64_lr0.01"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs64.py' \
-  --work-dir 'output/resnet34/bs64/lr0.01'\
+	--config 'configs/resnet/resnet34/lr0.01/resnet34_flowers_bs64.py' \
+	--work-dir 'output/resnet34/bs64/lr0.01'
 
 echo "Training resnet34_bs64_lr0.001"
 python tools/train.py \
-  --config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs64.py' \
-  --work-dir 'output/resnet34/bs64/lr0.001'\
+	--config 'configs/resnet/resnet34/lr0.001/resnet34_flowers_bs64.py' \
+	--work-dir 'output/resnet34/bs64/lr0.001'
 
 
 
